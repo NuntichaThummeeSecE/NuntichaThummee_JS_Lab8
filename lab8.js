06423602951099a776dd208f02631b9f8e483193
NuntichaThummeeSecE <n01687478@humber.ca>
2024-10-16 11:59:34 -0400

    try to use .getElementById only one time

--- lab8.js
+++ lab8.js
@@ -1,8 +1,17 @@
 //Task 1 click event : change to button text to Clicked//
-const createClicked = () => {
+
+//-- in this using .getElementById 2 times so I try to make it 1--//
+/*const createClicked = () => {
     document.getElementById(`clickEvent`).innerHTML = "Clicked!";
 }
 
-document.getElementById(`clickEvent`).addEventListener(`click`, createClicked);
+document.getElementById(`clickEvent`).addEventListener(`click`, createClicked);*/
 
-//Task 2 mouse events//
+const clickButton = document.getElementById(`clickEvent`);
+
+clickButton.addEventListener(`click`, () => {
+    clickButton.innerHTML = `Clicked!`;
+});
+
+
+
